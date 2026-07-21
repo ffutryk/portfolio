57d5e8dfc741df9e0110d07bda4c8a91aa2d6f54
ffutryk <ffutryk52@gmail.com>
2026-07-21 01:59:34 +0000

    feat: add and use JetBrains Mono font

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,19 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
+  fonts: [
+    {
+      provider: fontProviders.fontsource(),
+      name: 'JetBrains Mono',
+      cssVariable: '--font-jetbrains-mono',
+      fallbacks: ['monospace'],
+    },
+  ],
 });
